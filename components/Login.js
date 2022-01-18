51d29bea7d6e0767a619d85da8455a94f93fd0a6
Zhixing Hu <kennyyhhu@hotmail.com>
2022-01-17 19:22:16 -0800

    Add instruction

--- components/Login.js
+++ components/Login.js
@@ -1,11 +1,18 @@
 import Image from "next/image";
+import { useRouter } from "next/router";
 import { useMoralis } from 'react-moralis';
 
 function Login() {
+    const router = useRouter();
     const { authenticate, isInitializing } = useMoralis();
+    const addExtension = () => {
+        router.push({
+            pathname: 'https://metamask.io/download.html',
+        });
+    }
     return (
         <div className="bg-black relative">
-            <h1>I am the login screen</h1>
+            
             <div className="flex flex-col absolute z-50 h-4/6 w-full  items-center justify-center space-y-4">
                 <Image className="object-cover rounded-full bg-black"
                     src='https://i.pinimg.com/564x/39/89/ed/3989edc7fe67538cd40974324582abba.jpg' 
@@ -14,11 +21,19 @@ function Login() {
                 />
                 <button 
                     onClick={authenticate}
-                    className="bg-yellow-500 rounded-lg p-3 font-bold animate-pulse"
+                    className="bg-yellow-500 rounded-lg p-3 font-bold animate-bounce"
                 >
                     Login to the Metaverse
                 </button>
+                    
+                <button
+                    onClick={addExtension}
+                    className="bg-yellow-500 rounded-lg p-3 font-semibold animate-pulse"
+                >
+                    Add Metamask Extension
+                </button>
             </div>
+            
             <div className="w-full h-screen">
                 <Image 
                 src="https://links.papareact.com/55n" 
